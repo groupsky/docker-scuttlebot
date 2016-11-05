ec12d5b0040f6eff1b8a7aea97ff3ee6d44ed1eb
Geno Roupsky <geno@roupsky.name>
2016-11-05 17:05:15 +0200

    check if we're a new node or not and name and create an invite if new

--- scripts/run-sbot.sh
+++ scripts/run-sbot.sh
@@ -4,12 +4,23 @@ NODE_NAME=${NODE_NAME:-hackafe}
 
 echo Starting sbot: $HOST
 (
-    echo "sleeping 10s to generate manifest"; 
-    sleep 10; 
+    echo "sleeping 10s to generate manifest"
+    sleep 10
     NODE_ID=`sbot whoami | grep "id" | cut '-d"' -f4`
     echo "node id $NODE_ID"
-    echo "naming $NODE_NAME"
-    sbot publish --type about --about $NODE_ID --name $NODE_NAME
-    sbot invite.create 1
+
+    echo "lets see if we have existing db..."
+    LOG=`sbot logt about --limit 1`
+    echo $LOG
+    if [ "$LOG" -ne "" ]; then
+	echo "seems we already have data"
+    else
+	echo "new node let's name it"
+
+	echo "naming $NODE_NAME"
+	sbot publish --type about --about $NODE_ID --name $NODE_NAME
+	sbot invite.create 1
+
+    fi
 )&
 sbot server --host $HOST
