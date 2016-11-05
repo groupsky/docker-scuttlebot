39e023e737b174c3606c7afea2edaee5274ddbdd
Geno Roupsky <geno@roupsky.name>
2016-11-05 13:14:29 +0200

    name the host and create an invite

--- scripts/run-sbot.sh
+++ scripts/run-sbot.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+NODE_NAME=${NODE_NAME:-hackafe}
+
 echo Starting sbot: $HOST
-(echo "sleeping 10s to generate manifest"; sleep 10; sbot whoami)&
+(
+    echo "sleeping 10s to generate manifest"; 
+    sleep 10; 
+    NODE_ID=`sbot whoami | grep "id" | cut '-d"' -f4`
+    echo "node id $NODE_ID"
+    echo "naming $NODE_NAME"
+    sbot publish --type about --about $NODE_ID --name $NODE_NAME
+    sbot invite.create 1
+)&
 sbot server --host $HOST
